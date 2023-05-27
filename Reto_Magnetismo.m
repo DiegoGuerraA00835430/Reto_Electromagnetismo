a9373ecb75b74bf4700e08a1d45bbea51b6477f1
DiegoGuerraA00835430 <88684991+DiegoGuerraA00835430@users.noreply.github.com>
2023-05-26 22:30:24 -0600

    Update Reto_Magnetismo.m

--- Reto_Magnetismo.m
+++ Reto_Magnetismo.m
@@ -9,7 +9,6 @@
 
 
 % Parámetros de entrada
-n = 5;
 f1 = @(x) (x.^2);
 
 a1 = -1;
@@ -20,10 +19,16 @@ f2 = @(x) sin(x);
 a2 = 0;
 b2 = 2*pi;
 
-I = 1;
+%integral(f1,a1,b1)
+%[r1,tiempoIntegral1] =getFunction(a1,b1,f1,n)
+
+% integral(f2,a2,b2)
+%[r2,tiempoIntegral2] = getFunction(a2,b2,f2,n)
+
+I = -1;
 R = 5;
 x = 0;
-y = 0;
+y = 10;
 z = 0;
 
 [campo,tiempo] = getCampoMagnetico(I, R, n, x, y, z);
@@ -50,7 +55,7 @@ By = zeros(size(Y));
 Bz = zeros(size(Z));
 
 % Calcular el campo magnético en cada punto de la cuadrícula
-for i = 1:numel(X)
+for i = 1:numel(Y)
     campo_i = getCampoMagnetico(I, R, n, X(i), Y(i), Z(i));
     Bx(i) = campo_i(1);
     By(i) = campo_i(2);
@@ -58,7 +63,7 @@ for i = 1:numel(X)
 end
 
 % Graficar el campo magnético en un gráfico bidimensional
-quiver(X, Y, Bx, By);
+quiver(Y, Z, By, Bz);
 xlabel('x');
 ylabel('y');
 tiempoGraf = toc;
@@ -89,5 +94,20 @@ function [campo,tiempo] = getCampoMagnetico(I, R, n, x, y, z)
     end
 
     campo = B;
+    tiempo = toc;
+end
+
+function [valor,tiempo] = getFunction(a,b,f,n)
+    tic
+    lim = linspace(a,b,n);
+    valor = 0;
+    i = 1;
+    dx = (b-a)/n;
+    while( i<=n)
+        
+        valor = valor + (f(lim(i)) * dx);
+        i = i + 1;
+       
+    end
     tiempo = toc;
 end
